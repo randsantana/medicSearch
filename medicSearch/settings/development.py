7adb9b0fa75e82014c609286ae9bcede11b4a08d
randerson_santana <randerson_santana@estudante.sesisenai.org.br>
2024-07-04 21:39:42 -0300

    first commit

---
+++ medicSearch/settings/development.py
@@ -0,0 +1,23 @@
+from.settings import *
+DEBUG = True
+#chave secreta
+SECRET_KEY = 'dasda6e4g22e'
+#conectar no banco sqllite3
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+#conectar no banco mysql
+# DATABASE={
+#     'default': {
+#         'ENGINE': 'django.db.backends.MYSQL',
+#         'NAME': 'medicdb',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+# }
+# }
+ALLOEWD_HOSTS=['localhost', '127.0.0.1']
---
+++ medicSearch/settings/settings.py
@@ -0,0 +1,7 @@
+from pathlib import path
+LANGUAGE_CODE='pt-BR'
+TIME_ZONE='America/Sao_Paulo'
+import os
+STATIC_URL = '/static'
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
+ALLOEWD_HOSTS=['localhost', '127.0.0.1']
